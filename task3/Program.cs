d0c3dd1a060e17ba9effa383a6fbdb996d8cef0a
Anastasiya <piterskayaz@mail.ru>
2022-08-07 16:42:59 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу,  которая на вход принимает число и выдает, является ли число чётным
+int Usernumber = new int();
+Console.WriteLine("Введите чиcло: ");
+Usernumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введенное число чётное?");
+
+if (Usernumber%2 == 0)
+    {
+        Console.WriteLine("да");
+    }
+
+else 
+{
+    Console.WriteLine("нет");
+}
+
+
+
